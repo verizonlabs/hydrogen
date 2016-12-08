7b4fc3ca144cc21c100d5367cecffc4fa7f525b1
Tim Hansen <timothy.hansen@one.verizon.com>
2016-12-08 14:19:42 -0500

    .gitignore updated

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"github.com/verizonlabs/mesos-go/mesosproto"
 	"fmt"
 	"github.com/gogo/protobuf/proto"
+	"log"
+	"os"
+	"flag"
 )
 
+var (
+	master = flag.String("master", "127.0.0.1:5050", "Master address <ip:port>")
+	executorPath = flag.String("executor", "./example_executor", "Path to test executor.")
+)
+
+func init(){
+	flag.Parse()
+}
+
 func main(){
-	master := "172.17.8.101:5050"
+	if *master == "127.0.0.1:5050" {
+		*master = "172.17.8.101:5050"
+	}
 
 	// Make the scheduler
 	sched := scheduler.NewScheduler(executor.NewExecInfo())
@@ -22,11 +36,14 @@ func main(){
 	}
 
 	// scheduler, frameworkInfo, master string
-	driver, err := scheduler.NewSchedulerDriver(sched, &info, master)
+	driver, err := scheduler.NewSchedulerDriver(sched, &info, *master)
 
 	if err != nil {
 		fmt.Printf("Scheduler Driver failed to be created: %v", err)
 	}
 
-	driver.Start()
+	if status, err := driver.Start(); err != nil{
+		log.Fatalf("Framework stopped with status %s and error: %s\n", status.String(), err.Error())
+		os.Exit(-1)
+	}
 }
