b1d7a595aac9a04f2d85330197a790904ddfa9dc
Tim Hansen <timothy.hansen@one.verizon.com>
2016-12-12 14:37:56 -0500

    Add executor handler functions to break out how each event is processed.

--- executor/executor.go
+++ executor/executor.go
@@ -43,36 +43,6 @@ func NewExecutor(cfg *configuration) *executor {
 		unackedTasks:   make(map[mesos.TaskID]mesos.TaskInfo),
 		unackedUpdates: make(map[string]exec.Call_Update),
 		failedTasks:    make(map[mesos.TaskID]mesos.TaskStatus),
-		eventHandler: events.NewMux(
-			//TODO break all of these handlers out into their own unit
-			events.Handle(exec.Event_SUBSCRIBED, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle subscribed event
-				return nil
-			})),
-			events.Handle(exec.Event_LAUNCH, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle launch event
-				return nil
-			})),
-			events.Handle(exec.Event_KILL, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle kill event
-				return nil
-			})),
-			events.Handle(exec.Event_ACKNOWLEDGED, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle ack event
-				return nil
-			})),
-			events.Handle(exec.Event_MESSAGE, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle message event
-				return nil
-			})),
-			events.Handle(exec.Event_SHUTDOWN, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle shutdown event
-				return nil
-			})),
-			events.Handle(exec.Event_ERROR, events.HandlerFunc(func(e *exec.Event) error {
-				//TODO handle error event
-				return nil
-			})),
-		),
+		eventHandler:   NewExecutorHandler(),
 	}
 }
--- main.go
+++ main.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"flag"
-	"os"
 	"github.com/verizonlabs/sprint/scheduler"
+	"os"
 )
 
 func main() {
