cae542ec699dcfecacb895a0847eae9ab2c37ee6
Aaron Wood <aaronjwood@gmail.com>
2016-12-13 16:35:42 -0500

    Change base configuration interface a bit and alter tests

--- scheduler/configuration.go
+++ scheduler/configuration.go
@@ -10,6 +10,13 @@ type baseConfiguration interface {
 	Initialize(fs *flag.FlagSet)
 	GetName() string
 	GetCheckpointing() *bool
+	GetPrincipal() string
+	GetCommand() string
+	GetUris() []mesos.CommandInfo_URI
+	GetEndpoint() string
+	GetTimeout() time.Duration
+	GetReviveBurst() int
+	GetReviveWait() time.Duration
 }
 
 // Configuration for the scheduler, populated by user-supplied flags.
@@ -43,4 +50,32 @@ func (c *Configuration) GetName() string {
 
 func (c *Configuration) GetCheckpointing() *bool {
 	return &c.checkpointing
+}
+
+func (c *Configuration) GetPrincipal() string {
+	return c.principal
+}
+
+func (c *Configuration) GetCommand() string {
+	return c.command
+}
+
+func (c *Configuration) GetUris() []mesos.CommandInfo_URI {
+	return c.uris
+}
+
+func (c *Configuration) GetTimeout() time.Duration {
+	return c.timeout
+}
+
+func (c *Configuration) GetEndpoint() string {
+	return c.endpoint
+}
+
+func (c *Configuration) GetReviveBurst() int {
+	return c.reviveBurst
+}
+
+func (c *Configuration) GetReviveWait() time.Duration {
+	return c.reviveWait
 }
--- scheduler/configuration_test.go
+++ scheduler/configuration_test.go
@@ -2,41 +2,68 @@ package scheduler
 
 import (
 	"flag"
+	"github.com/verizonlabs/mesos-go"
 	"testing"
 	"time"
 )
 
 // Mocked configuration
 type mockConfiguration struct {
-	name          string
-	checkpointing *bool
+	cfg Configuration
 }
 
 func (m *mockConfiguration) Initialize(fs *flag.FlagSet) {
-	checkpointing := true
-
-	m.name = "Test"
-	m.checkpointing = &checkpointing
+	m.cfg.name = "Sprint"
+	m.cfg.checkpointing = true
+	m.cfg.command = ""
+	m.cfg.endpoint = "http://127.0.0.1:5050/api/v1/scheduler"
+	m.cfg.principal = "Sprint"
+	m.cfg.reviveBurst = 3
+	m.cfg.reviveWait = 1 * time.Second
+	m.cfg.timeout = 20 * time.Second
 }
 
 func (m *mockConfiguration) GetName() string {
-	return m.name
+	return m.cfg.name
 }
 
 func (m *mockConfiguration) GetCheckpointing() *bool {
-	return m.checkpointing
+	return &m.cfg.checkpointing
 }
 
-var (
-	endpoint      = "http://127.0.0.1:5050/api/v1/scheduler"
-	name          = "Sprint"
-	checkpointing = true
-	principal     = "Sprint"
-	command       = ""
-	timeout       = 20 * time.Second
-	reviveBurst   = 3
-	reviveWait    = 1 * time.Second
-)
+func (m *mockConfiguration) GetPrincipal() string {
+	return m.cfg.principal
+}
+
+func (m *mockConfiguration) GetCommand() string {
+	return m.cfg.command
+}
+
+func (m *mockConfiguration) GetUris() []mesos.CommandInfo_URI {
+	return m.cfg.uris
+}
+
+func (m *mockConfiguration) GetTimeout() time.Duration {
+	return m.cfg.timeout
+}
+
+func (m *mockConfiguration) GetEndpoint() string {
+	return m.cfg.endpoint
+}
+
+func (m *mockConfiguration) GetReviveBurst() int {
+	return m.cfg.reviveBurst
+}
+
+func (m *mockConfiguration) GetReviveWait() time.Duration {
+	return m.cfg.reviveWait
+}
+
+var cfg baseConfiguration
+
+func init() {
+	cfg = new(mockConfiguration)
+}
 
 // Tests setting up default configuration values
 func TestConfiguration_Initialize(t *testing.T) {
@@ -47,28 +74,28 @@ func TestConfiguration_Initialize(t *testing.T) {
 	config := new(Configuration)
 	config.Initialize(fs)
 
-	if config.endpoint != endpoint {
+	if config.endpoint != cfg.GetEndpoint() {
 		t.Fatal("Invalid endpoint")
 	}
-	if config.checkpointing != checkpointing {
+	if config.checkpointing != *cfg.GetCheckpointing() {
 		t.Fatal("Checkpointing is disabled")
 	}
-	if config.command != command {
+	if config.command != cfg.GetCommand() {
 		t.Fatal("Invalid command")
 	}
-	if config.name != name {
+	if config.name != cfg.GetName() {
 		t.Fatal("Invalid framework name")
 	}
-	if config.principal != principal {
+	if config.principal != cfg.GetPrincipal() {
 		t.Fatal("Invalid framework principal")
 	}
-	if config.timeout != timeout {
+	if config.timeout != cfg.GetTimeout() {
 		t.Fatal("Timeout value is not consistent")
 	}
-	if config.reviveBurst != reviveBurst {
+	if config.reviveBurst != cfg.GetReviveBurst() {
 		t.Fatal("Revive burst value is not consistent")
 	}
-	if config.reviveWait != reviveWait {
+	if config.reviveWait != cfg.GetReviveWait() {
 		t.Fatal("Revive wait duration is not consistent")
 	}
 }
