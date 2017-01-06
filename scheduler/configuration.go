c3bf9c2bdddb84d49bb93bd9fb2c2ee5cbb93f2d
Aaron Wood <aaronjwood@gmail.com>
2017-01-06 13:56:56 -0500

    Add more configuration to support the new executor server

--- scheduler/configuration.go
+++ scheduler/configuration.go
@@ -18,20 +18,26 @@ type configuration interface {
 	ReviveBurst() int
 	ReviveWait() time.Duration
 	MaxRefuse() time.Duration
+	ExecutorSrvPath() string
+	ExecutorSrvPort() int
 }
 
 // Configuration for the scheduler, populated by user-supplied flags.
 type SprintConfiguration struct {
-	endpoint      string
-	name          string
-	checkpointing bool
-	principal     string
-	uris          []mesos.CommandInfo_URI
-	command       string
-	timeout       time.Duration
-	reviveBurst   int
-	reviveWait    time.Duration
-	maxRefuse     time.Duration
+	endpoint        string
+	name            string
+	checkpointing   bool
+	principal       string
+	uris            []mesos.CommandInfo_URI
+	command         string
+	timeout         time.Duration
+	reviveBurst     int
+	reviveWait      time.Duration
+	maxRefuse       time.Duration
+	executorSrvCert string
+	executorSrvKey  string
+	executorSrvPath string
+	executorSrvPort int
 }
 
 // Applies values to the various configurations from user-supplied flags.
@@ -45,6 +51,10 @@ func (c *SprintConfiguration) Initialize(fs *flag.FlagSet) *SprintConfiguration 
 	fs.IntVar(&c.reviveBurst, "revive.burst", 3, "Number of revive messages that may be sent in a burst within revive-wait period")
 	fs.DurationVar(&c.reviveWait, "revive.wait", 1*time.Second, "Wait this long to fully recharge revive-burst quota")
 	fs.DurationVar(&c.maxRefuse, "maxRefuse", 5*time.Second, "Max length of time to refuse future offers")
+	fs.StringVar(&c.executorSrvCert, "server.executor.cert", "tls.crt", "TLS certificate")
+	fs.StringVar(&c.executorSrvKey, "server.executor.key", "tls.key", "TLS key")
+	fs.StringVar(&c.executorSrvPath, "server.executor.path", "executor", "Path to the executor binary")
+	fs.IntVar(&c.executorSrvPort, "server.executor.port", 8081, "Executor server listen port")
 
 	return c
 }
@@ -87,4 +97,20 @@ func (c *SprintConfiguration) ReviveWait() time.Duration {
 
 func (c *SprintConfiguration) MaxRefuse() time.Duration {
 	return c.maxRefuse
+}
+
+func (c *SprintConfiguration) ExecutorSrvCert() string {
+	return c.executorSrvCert
+}
+
+func (c *SprintConfiguration) ExecutorSrvKey() string {
+	return c.executorSrvKey
+}
+
+func (c *SprintConfiguration) ExecutorSrvPath() string {
+	return c.executorSrvPath
+}
+
+func (c *SprintConfiguration) ExecutorSrvPort() int {
+	return c.executorSrvPort
 }
