1b69423643310371c6f3ea5b43d1fd9ab3c0db88
Tim Hansen <timothy.hansen@one.verizon.com>
2017-01-19 17:28:08 -0500

    Add proper struct for applications. Add comments. Add new factory functions for API configuration.

--- scheduler/api/api.go
+++ scheduler/api/api.go
@@ -3,10 +3,11 @@ package api
 import (
 	"encoding/json"
 	"fmt"
-	"html"
 	"io/ioutil"
 	"log"
+	"mesos-sdk"
 	"net/http"
+	"strconv"
 )
 
 const (
@@ -14,31 +15,100 @@ const (
 	deployEndpoint = "/deploy"
 )
 
-type Message struct {
-	Test string
+/*
+This struct represents the possible application configuration options for an end-user of sprint.
+*/
+type ApplicationJSON struct {
+	Name        string
+	Resources   []mesos.Resource
+	Command     *mesos.CommandInfo
+	Container   *mesos.ContainerInfo
+	HealthCheck *mesos.HealthCheck
+	Labels      *mesos.Labels
 }
 
+/*
+Configuration for our API
+*/
 type ApiConfiguration struct {
 	port   int
-	handle map[string]http.HandlerFunc // route -> handler func for that route
+	handle *map[string]http.HandlerFunc // route -> handler func for that route
 }
 
+/*
+Getter to return our port as an int
+*/
+func (a *ApiConfiguration) Port() int {
+	return a.port
+}
+
+/*
+Getter to return a string of our port.
+*/
+func (a *ApiConfiguration) PortAsString() string {
+	return strconv.Itoa(a.port)
+}
+
+/*
+Getter to return our map of handles
+*/
+func (a *ApiConfiguration) Handle() *map[string]http.HandlerFunc {
+	return a.handle
+}
+
+/*
+Return a default new ApiConfiguration.
+*/
+func NewApiConfiguration() *ApiConfiguration {
+	return &ApiConfiguration{
+		handle: defautlHandleFunctions(),
+		port:   8080,
+	}
+}
+
+/*
+Set our default API handler routes here.
+*/
+func defautlHandleFunctions() *map[string]http.HandlerFunc {
+	handlers := make(map[string]http.HandlerFunc, 4)
+	handlers[baseUrl+deployEndpoint] = deploy
+	return &handlers
+}
+
+/*
+Default Run method with no config
+*/
 func RunAPI() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	cfg := NewApiConfiguration()
+	// Iterate through all methods and setup endpoints.
+	for route, handle := range *cfg.handle {
+		setupHandle(route, handle)
+	}
 
-	setupHandle(baseUrl+deployEndpoint, deploy)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
-
+	log.Fatal(http.ListenAndServe(":"+cfg.PortAsString(), nil))
 }
 
+/*
+Run function that takes a given ApiConfiguration to configure the API server.
+*/
+func Run(cfg *ApiConfiguration) {
+	for route, handle := range *cfg.handle {
+		setupHandle(route, handle)
+	}
+
+	log.Fatal(http.ListenAndServe(":"+cfg.PortAsString(), nil))
+}
+
+/*
+Functional way to add a handler.
+*/
 func setupHandle(route string, handle func(w http.ResponseWriter, r *http.Request)) {
 	http.HandleFunc(route, handle)
 }
 
-// /v1/api/deploy
+/*
+Deploy endpoint will parse given JSON and create a given TaskInfo for the scheduler to execute.
+*/
 func deploy(w http.ResponseWriter, r *http.Request) {
 	// We don't want to allow any other methods.
 	if r.Method == "POST" {
@@ -49,14 +119,14 @@ func deploy(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		var m Message
+		var m ApplicationJSON
 		err = json.Unmarshal(dec, &m)
 		if err != nil {
 			fmt.Fprintf(w, err.Error())
 			return
 		}
 
-		fmt.Fprintf(w, "%v", m)
+		fmt.Fprintf(w, "%v", m) // This is where we would form our Task info.
 	} else {
 		fmt.Fprintf(w, r.Method+" is not allowed on this endpoint.")
 	}
