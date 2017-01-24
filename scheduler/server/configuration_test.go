d6ddd5c2ed736d6089dd473ded9b93328f8966bb
Aaron Wood <aaronjwood@gmail.com>
2017-01-24 17:34:38 -0500

    Add test for checking the shared HTTP server

--- scheduler/server/configuration_test.go
+++ scheduler/server/configuration_test.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"crypto/tls"
+	"net/http"
+	"reflect"
 	"testing"
 )
 
@@ -30,6 +33,33 @@ func TestServerConfiguration_Protocol(t *testing.T) {
 
 	if serverCfg.Protocol() != "http" {
 		t.Fatal("Server protocol is incorrect")
+	}
+}
+
+// Checks to see if our HTTP server is configured properly.
+func TestServerConfiguration_Server(t *testing.T) {
+	t.Parallel()
+
+	srv := &http.Server{
+		TLSConfig: &tls.Config{
+			// Use only the most secure protocol version.
+			MinVersion: tls.VersionTLS12,
+			// Use very strong crypto curves.
+			CurvePreferences:         []tls.CurveID{tls.CurveP521, tls.CurveP384, tls.CurveP256},
+			PreferServerCipherSuites: true,
+			// Use very strong cipher suites (order is important here!)
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256, // Required for HTTP/2 support.
+				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(serverCfg.Server(), srv) {
+		t.Fatal("HTTP server is not initialized correctly")
 	}
 }
 
