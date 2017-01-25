76d119e7fa187bb86275829ef939d9417cd59a7a
Aaron Wood <aaronjwood@gmail.com>
2017-01-25 11:25:46 -0500

    Add benchmarks for the server configuration

--- scheduler/server/configuration_test.go
+++ scheduler/server/configuration_test.go
@@ -18,6 +18,13 @@ func TestServerConfiguration_Cert(t *testing.T) {
 	}
 }
 
+// Measure performance of getting the path to the TLS certificate.
+func BenchmarkServerConfiguration_Cert(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		serverCfg.Cert()
+	}
+}
+
 // Make sure we get our TLS key properly.
 func TestServerConfiguration_Key(t *testing.T) {
 	t.Parallel()
@@ -27,12 +34,26 @@ func TestServerConfiguration_Key(t *testing.T) {
 	}
 }
 
+// Measure performance of getting the path to the TLS key.
+func BenchmarkServerConfiguration_Key(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		serverCfg.Key()
+	}
+}
+
 // Make sure our protocol is set correctly.
 func TestServerConfiguration_Protocol(t *testing.T) {
 	t.Parallel()
 
 	if serverCfg.Protocol() != "http" {
 		t.Fatal("Server protocol is incorrect")
+	}
+}
+
+// Measure performance of determining the protocol to be used.
+func BenchmarkServerConfiguration_Protocol(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		serverCfg.Protocol()
 	}
 }
 
@@ -63,11 +84,25 @@ func TestServerConfiguration_Server(t *testing.T) {
 	}
 }
 
+// Measure performance of getting our shared HTTP server.
+func BenchmarkServerConfiguration_Server(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		serverCfg.Server()
+	}
+}
+
 // Make sure TLS defaults to off.
 func TestServerConfiguration_TLS(t *testing.T) {
 	t.Parallel()
 
 	if serverCfg.TLS() {
 		t.Fatal("TLS has the wrong default setting")
+	}
+}
+
+// Measure performance of determining if TLS is enabled or not.
+func BenchmarkServerConfiguration_TLS(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		serverCfg.TLS()
 	}
 }
