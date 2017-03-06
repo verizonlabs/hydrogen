f1fd232a4853321b64ae72a02598743030e4471e
Aaron Wood <aaronjwood@gmail.com>
2017-03-06 15:14:09 -0500

    Executor config needs to be rethough/created in the future

--- executor/main/main.go
+++ executor/main/main.go
@@ -7,10 +7,7 @@ import (
 
 // Main function will wire up all other dependencies for the executor and setup top-level configuration.
 func main() {
-	//cfg := new(exec.ExecutorConfiguration).Initialize()
-	//flag.Parse()
 	c := client.NewClient("http://68.128.154.89:5050/api/v1/executor")
 	e := exec.NewDefaultExecutor(c)
 	e.Run()
-
 }
