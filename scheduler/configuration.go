463056eaca59fae3c23e32af6db1ed7aaf8b4302
Aaron Wood <aaronjwood@gmail.com>
2017-03-29 11:34:19 -0400

    Make some of the flag descriptions easier to read

--- scheduler/configuration.go
+++ scheduler/configuration.go
@@ -46,7 +46,8 @@ func (c *SchedulerConfiguration) Initialize() *SchedulerConfiguration {
 	flag.StringVar(&c.Role, "role", "*", "Framework role")
 	flag.BoolVar(&c.Checkpointing, "checkpointing", true, "Enable or disable checkpointing")
 	flag.StringVar(&c.Principal, "principal", "Sprint", "Framework principal")
-	flag.StringVar(&c.StorageEndpoints, "persistence.endpoints", "http://127.0.0.1:2379", "Comma-separated list of storage endpoints")
+	flag.StringVar(&c.StorageEndpoints, "persistence.endpoints", "http://127.0.0.1:2379", `Comma-separated list of
+											       storage endpoints`)
 	flag.DurationVar(&c.StorageTimeout, "persistence.timeout", 2*time.Second, "Initial storage system connection timeout")
 	flag.DurationVar(&c.StorageKeepaliveTime, "persistence.keepalive.time", 30*time.Second, `After a duration of this time
 												 if the client doesn't see any activity
@@ -58,13 +59,16 @@ func (c *SchedulerConfiguration) Initialize() *SchedulerConfiguration {
 												       even after that the connection is closed`)
 	flag.Float64Var(&c.Failover, "failover", time.Minute.Seconds(), "Framework failover timeout")
 	flag.StringVar(&c.Hostname, "hostname", "", "The framework's hostname")
-	flag.DurationVar(&c.SubscribeRetry, "subscribe.retry", 2*time.Second, "Controls the interval at which subscribe calls will be retried")
+	flag.DurationVar(&c.SubscribeRetry, "subscribe.retry", 2*time.Second, `Controls the interval at which subscribe
+									       calls will be retried`)
 	flag.DurationVar(&c.ReconcileInterval, "reconcile.interval", 15*time.Minute, "How often periodic reconciling happens")
 	flag.StringVar(&c.LeaderIP, "ha.leader.ip", "", "IP address of the node where this framework is running")
 	flag.IntVar(&c.LeaderServerPort, "ha.leader.server.port", 8082, "Port that the leader server listens on")
 	flag.StringVar(&c.LeaderAddressFamily, "ha.leader.address.family", "tcp4", "tcp4, tcp6, or tcp for dual stack")
-	flag.DurationVar(&c.LeaderRetryInterval, "ha.leader.retry", 2*time.Second, "How long to wait before retrying the leader election process")
-	flag.DurationVar(&c.LeaderServerRetry, "ha.leader.server.retry", 2*time.Second, "How long to wait before accepting connections from clients after an error")
+	flag.DurationVar(&c.LeaderRetryInterval, "ha.leader.retry", 2*time.Second, `How long to wait before retrying
+										    the leader election process`)
+	flag.DurationVar(&c.LeaderServerRetry, "ha.leader.server.retry", 2*time.Second, `How long to wait before accepting
+											 connections from clients after an error`)
 
 	return c
 }
