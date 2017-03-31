ed7df511e369e3cd337b5f2a4570dd26a848bd17
Aaron Wood <aaronjwood@gmail.com>
2017-03-31 11:09:19 -0400

    Reuse logic for determining what methods to allow and for returning an error response back on disallowed methods

--- scheduler/api/api.go
+++ scheduler/api/api.go
@@ -110,67 +110,11 @@ func (a *ApiServer) RunAPI(handlers map[string]http.HandlerFunc) {
 	}
 }
 
-// Deploys a given application from parsed JSON
-func (a *ApiServer) deploy(w http.ResponseWriter, r *http.Request) {
+func (a *ApiServer) methodFilter(w http.ResponseWriter, r *http.Request, method string, ops func()) {
 	switch r.Method {
-	case "POST":
+	case method:
 		{
-			dec, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:  response.FAILED,
-					Message: err.Error(),
-				})
-				return
-			}
-
-			var m taskbuilder.ApplicationJSON
-			err = json.Unmarshal(dec, &m)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:  response.FAILED,
-					Message: err.Error(),
-				})
-				return
-			}
-
-			task, err := builder.Application(&m, a.logger)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:   response.FAILED,
-					TaskName: task.GetName(),
-					Message:  err.Error(),
-				})
-				return
-			}
-
-			// If we have any filters, let the resource manager know.
-			if len(m.Filters) > 0 {
-				if err := a.resourceMgr.AddFilter(task, m.Filters); err != nil {
-					json.NewEncoder(w).Encode(response.Deploy{
-						Status:   response.FAILED,
-						TaskName: task.GetName(),
-						Message:  err.Error(),
-					})
-					return
-				}
-
-			}
-
-			if err := a.taskMgr.Add(task); err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:   response.FAILED,
-					TaskName: task.GetName(),
-					Message:  err.Error(),
-				})
-				return
-			}
-			a.sched.Revive()
-
-			json.NewEncoder(w).Encode(response.Deploy{
-				Status:   response.QUEUED,
-				TaskName: task.GetName(),
-			})
+			ops()
 		}
 	default:
 		{
@@ -180,203 +124,224 @@ func (a *ApiServer) deploy(w http.ResponseWriter, r *http.Request) {
 			})
 		}
 	}
+}
 
+// Deploys a given application from parsed JSON
+func (a *ApiServer) deploy(w http.ResponseWriter, r *http.Request) {
+	a.methodFilter(w, r, "POST", func() {
+		dec, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:  response.FAILED,
+				Message: err.Error(),
+			})
+			return
+		}
+
+		var m taskbuilder.ApplicationJSON
+		err = json.Unmarshal(dec, &m)
+		if err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:  response.FAILED,
+				Message: err.Error(),
+			})
+			return
+		}
+
+		task, err := builder.Application(&m, a.logger)
+		if err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:   response.FAILED,
+				TaskName: task.GetName(),
+				Message:  err.Error(),
+			})
+			return
+		}
+
+		// If we have any filters, let the resource manager know.
+		if len(m.Filters) > 0 {
+			if err := a.resourceMgr.AddFilter(task, m.Filters); err != nil {
+				json.NewEncoder(w).Encode(response.Deploy{
+					Status:   response.FAILED,
+					TaskName: task.GetName(),
+					Message:  err.Error(),
+				})
+				return
+			}
+
+		}
+
+		if err := a.taskMgr.Add(task); err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:   response.FAILED,
+				TaskName: task.GetName(),
+				Message:  err.Error(),
+			})
+			return
+		}
+		a.sched.Revive()
+
+		json.NewEncoder(w).Encode(response.Deploy{
+			Status:   response.QUEUED,
+			TaskName: task.GetName(),
+		})
+	})
 }
 
 func (a *ApiServer) update(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "PUT":
-		{
-			dec, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:  response.FAILED,
-					Message: err.Error(),
-				})
-				return
-			}
-
-			var m taskbuilder.ApplicationJSON
-			err = json.Unmarshal(dec, &m)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:  response.FAILED,
-					Message: err.Error(),
-				})
-				return
-			}
-
-			// Check if this task already exists
-			taskToKill, err := a.taskMgr.Get(&m.Name)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:   response.FAILED,
-					TaskName: taskToKill.GetName(),
-					Message:  err.Error(),
-				})
-				return
-			}
-
-			task, err := builder.Application(&m, a.logger)
-
-			a.taskMgr.Set(sdkTaskManager.UNKNOWN, task)
-			a.sched.Kill(taskToKill.GetTaskId(), taskToKill.GetAgentId())
-			a.sched.Revive()
-
-			json.NewEncoder(w).Encode(response.Deploy{
-				Status:  response.UPDATE,
-				Message: fmt.Sprintf("Updating %v", task.GetName()),
-			})
-		}
-	default:
-		{
+	a.methodFilter(w, r, "PUT", func() {
+		dec, err := ioutil.ReadAll(r.Body)
+		if err != nil {
 			json.NewEncoder(w).Encode(response.Deploy{
 				Status:  response.FAILED,
-				Message: r.Method + " is not allowed on this endpoint.",
+				Message: err.Error(),
 			})
+			return
 		}
-	}
+
+		var m taskbuilder.ApplicationJSON
+		err = json.Unmarshal(dec, &m)
+		if err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:  response.FAILED,
+				Message: err.Error(),
+			})
+			return
+		}
+
+		// Check if this task already exists
+		taskToKill, err := a.taskMgr.Get(&m.Name)
+		if err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:   response.FAILED,
+				TaskName: taskToKill.GetName(),
+				Message:  err.Error(),
+			})
+			return
+		}
+
+		task, err := builder.Application(&m, a.logger)
+
+		a.taskMgr.Set(sdkTaskManager.UNKNOWN, task)
+		a.sched.Kill(taskToKill.GetTaskId(), taskToKill.GetAgentId())
+		a.sched.Revive()
+
+		json.NewEncoder(w).Encode(response.Deploy{
+			Status:  response.UPDATE,
+			Message: fmt.Sprintf("Updating %v", task.GetName()),
+		})
+	})
 }
 
 func (a *ApiServer) kill(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "POST":
-		{
-			dec, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				return
-			}
-
-			var m taskbuilder.KillJson
-			err = json.Unmarshal(dec, &m)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:  response.FAILED,
-					Message: err.Error(),
-				})
-				return
-			}
-
-			// Make sure we have a name to look up
-			if m.Name != nil {
-				// Look up task in task manager
-				t, err := a.taskMgr.Get(m.Name)
-				if err != nil {
-					json.NewEncoder(w).Encode(response.Kill{Status: response.NOTFOUND, TaskName: *m.Name})
-					return
-				}
-				// Get all tasks in RUNNING state.
-				running, _ := a.taskMgr.GetState(mesos_v1.TaskState_TASK_RUNNING)
-				// If we get an error, it means no tasks are currently in the running state.
-				// We safely ignore this- the range over the empty list will be skipped regardless.
-
-				// Check if our task is in the list of RUNNING tasks.
-				for _, task := range running {
-					// If it is, then send the kill signal.
-					if task.GetName() == t.GetName() {
-						// First Kill call to the mesos-master.
-						_, err := a.sched.Kill(t.GetTaskId(), t.GetAgentId())
-						if err != nil {
-							// If it fails, try to kill it again.
-							resp, err := a.sched.Kill(t.GetTaskId(), t.GetAgentId())
-							if err != nil {
-								// We've tried twice and still failed.
-								// Send back an error message.
-								json.NewEncoder(w).Encode(
-									response.Kill{
-										Status:   response.FAILED,
-										TaskName: *m.Name,
-										Message:  "Response Status to Kill: " + resp.Status,
-									},
-								)
-								return
-							}
-						}
-						// Our kill call has worked, delete it from the task queue.
-						a.taskMgr.Delete(t)
-						// Response appropriately.
-						json.NewEncoder(w).Encode(response.Kill{Status: response.KILLED, TaskName: *m.Name})
-						return
-					}
-				}
-				// If we get here, our task isn't in the list of RUNNING tasks.
-				// Delete it from the queue regardless.
-				// We run into this case if a task is flapping or unable to launch
-				// or get an appropriate offer.
-				a.taskMgr.Delete(t)
-				json.NewEncoder(w).Encode(response.Kill{Status: response.KILLED, TaskName: *m.Name})
-				return
-			}
-			// If we get here, there was no name passed in and the kill function failed.
-			json.NewEncoder(w).Encode(response.Kill{Status: response.FAILED, TaskName: *m.Name})
+	a.methodFilter(w, r, "POST", func() {
+		dec, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			return
 		}
-	default:
-		{
+
+		var m taskbuilder.KillJson
+		err = json.Unmarshal(dec, &m)
+		if err != nil {
 			json.NewEncoder(w).Encode(response.Deploy{
 				Status:  response.FAILED,
-				Message: r.Method + " is not allowed on this endpoint.",
+				Message: err.Error(),
 			})
+			return
 		}
-	}
 
+		// Make sure we have a name to look up
+		if m.Name != nil {
+			// Look up task in task manager
+			t, err := a.taskMgr.Get(m.Name)
+			if err != nil {
+				json.NewEncoder(w).Encode(response.Kill{Status: response.NOTFOUND, TaskName: *m.Name})
+				return
+			}
+			// Get all tasks in RUNNING state.
+			running, _ := a.taskMgr.GetState(mesos_v1.TaskState_TASK_RUNNING)
+			// If we get an error, it means no tasks are currently in the running state.
+			// We safely ignore this- the range over the empty list will be skipped regardless.
+
+			// Check if our task is in the list of RUNNING tasks.
+			for _, task := range running {
+				// If it is, then send the kill signal.
+				if task.GetName() == t.GetName() {
+					// First Kill call to the mesos-master.
+					_, err := a.sched.Kill(t.GetTaskId(), t.GetAgentId())
+					if err != nil {
+						// If it fails, try to kill it again.
+						resp, err := a.sched.Kill(t.GetTaskId(), t.GetAgentId())
+						if err != nil {
+							// We've tried twice and still failed.
+							// Send back an error message.
+							json.NewEncoder(w).Encode(
+								response.Kill{
+									Status:   response.FAILED,
+									TaskName: *m.Name,
+									Message:  "Response Status to Kill: " + resp.Status,
+								},
+							)
+							return
+						}
+					}
+					// Our kill call has worked, delete it from the task queue.
+					a.taskMgr.Delete(t)
+					// Response appropriately.
+					json.NewEncoder(w).Encode(response.Kill{Status: response.KILLED, TaskName: *m.Name})
+					return
+				}
+			}
+			// If we get here, our task isn't in the list of RUNNING tasks.
+			// Delete it from the queue regardless.
+			// We run into this case if a task is flapping or unable to launch
+			// or get an appropriate offer.
+			a.taskMgr.Delete(t)
+			json.NewEncoder(w).Encode(response.Kill{Status: response.KILLED, TaskName: *m.Name})
+			return
+		}
+		// If we get here, there was no name passed in and the kill function failed.
+		json.NewEncoder(w).Encode(response.Kill{Status: response.FAILED, TaskName: *m.Name})
+	})
 }
 
 // TODO (tim): Get state of mesos task and return it.
 func (a *ApiServer) stats(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		{
-			name := r.URL.Query().Get("name")
+	a.methodFilter(w, r, "GET", func() {
+		name := r.URL.Query().Get("name")
 
-			_, err := a.taskMgr.Get(&name)
-			if err != nil {
-				fmt.Fprintf(w, "Task not found, error %v", err.Error())
-				return
-			}
+		_, err := a.taskMgr.Get(&name)
+		if err != nil {
+			fmt.Fprintf(w, "Task not found, error %v", err.Error())
+			return
 		}
-	default:
-		{
-			json.NewEncoder(w).Encode(response.Deploy{
-				Status:  response.FAILED,
-				Message: r.Method + " is not allowed on this endpoint.",
-			})
-		}
-	}
+	})
 }
 
 // Status endpoint lets the end-user know about the TASK_STATUS of their task.
 func (a *ApiServer) state(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		{
-			name := r.URL.Query().Get("name")
+	a.methodFilter(w, r, "GET", func() {
+		name := r.URL.Query().Get("name")
 
-			_, err := a.taskMgr.Get(&name)
-			if err != nil {
-				json.NewEncoder(w).Encode(response.Deploy{
-					Status:  response.FAILED,
-					Message: err.Error(),
-				})
-				return
-			}
-			queued, err := a.taskMgr.GetState(sdkTaskManager.STAGING)
-			if err != nil {
-				a.logger.Emit(logging.INFO, err.Error())
-			}
-
-			for _, task := range queued {
-				if task.GetName() == name {
-					json.NewEncoder(w).Encode(response.Kill{Status: response.QUEUED, TaskName: name})
-				}
-			}
-
-			json.NewEncoder(w).Encode(response.Kill{Status: response.LAUNCHED, TaskName: name})
-
+		_, err := a.taskMgr.Get(&name)
+		if err != nil {
+			json.NewEncoder(w).Encode(response.Deploy{
+				Status:  response.FAILED,
+				Message: err.Error(),
+			})
+			return
 		}
-	default:
-		{
-			fmt.Fprintf(w, r.Method+" is not allowed on this endpoint.")
+		queued, err := a.taskMgr.GetState(sdkTaskManager.STAGING)
+		if err != nil {
+			a.logger.Emit(logging.INFO, err.Error())
 		}
-	}
 
+		for _, task := range queued {
+			if task.GetName() == name {
+				json.NewEncoder(w).Encode(response.Kill{Status: response.QUEUED, TaskName: name})
+			}
+		}
+
+		json.NewEncoder(w).Encode(response.Kill{Status: response.LAUNCHED, TaskName: name})
+	})
 }
