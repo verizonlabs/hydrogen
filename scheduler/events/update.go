7bf0af3c30545bb4c05bbcfedae0b2488015688b
Aaron Wood <aaronjwood@gmail.com>
2017-05-08 17:33:47 -0400

    Revert logging error level

--- scheduler/events/update.go
+++ scheduler/events/update.go
@@ -39,7 +39,7 @@ func (s *SprintEventController) Update(updateEvent *mesos_v1_scheduler.Event_Upd
 		// If there's an error, fallback to the regular policy.
 		policy, err := s.taskmanager.CheckPolicy(task)
 		if err != nil {
-			s.logger.Emit(logging.ERROR, err.Error())
+			s.logger.Emit(logging.INFO, err.Error())
 			// set default policy, we should never get here, this would mean an error in serialization or our api.
 			s.taskmanager.AddPolicy(apiManager.DEFAULT_RETRY_POLICY, task)
 		}
