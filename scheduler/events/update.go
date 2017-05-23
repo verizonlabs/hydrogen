5558fe43fb0887ea2bb37e5f1e675171d72070ca
Aaron Wood <aaronjwood@gmail.com>
2017-05-23 12:09:10 -0400

    Better logging for various events, reuse as many cached vars as possible throughout Update()

--- scheduler/events/update.go
+++ scheduler/events/update.go
@@ -14,6 +14,7 @@ import (
 //
 func (s *SprintEventController) Update(updateEvent *mesos_v1_scheduler.Event_Update) {
 	status := updateEvent.GetStatus()
+	agentId := status.GetAgentId()
 	taskId := status.GetTaskId()
 	task, err := s.taskmanager.GetById(taskId)
 	if err != nil {
@@ -22,63 +23,79 @@ func (s *SprintEventController) Update(updateEvent *mesos_v1_scheduler.Event_Upd
 		// NOTE (tim): Do we want to keep deleted task history for a certain amount of time
 		// before it's deleted? We would record status updates after it's killed here.
 		// ACK update, return.
-		s.scheduler.Acknowledge(status.GetAgentId(), status.GetTaskId(), status.GetUuid())
+		s.scheduler.Acknowledge(agentId, taskId, status.GetUuid())
 		return
 	}
 
 	state := status.GetState()
 	message := status.GetMessage()
-	agentId := status.GetAgentId()
+	taskIdVal := taskId.GetValue()
+	agentIdVal := agentId.GetValue()
+
 	s.taskmanager.Set(state, task)
 
 	switch state {
 	case mesos_v1.TaskState_TASK_FAILED:
-		s.logger.Emit(logging.ERROR, message)
+		s.logger.Emit(logging.ERROR, "Task %s failed: %s", taskIdVal, message)
 		s.reschedule(task)
 	case mesos_v1.TaskState_TASK_STAGING:
 		// NOP, keep task set to "launched".
-		s.logger.Emit(logging.INFO, message)
+		s.logger.Emit(logging.INFO, "Task %s is staging: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_DROPPED:
 		// Transient error, we should retry launching. Taskinfo is fine.
-		s.logger.Emit(logging.INFO, message)
+		s.logger.Emit(logging.INFO, "Task %s dropped: %s", taskIdVal, message)
 		s.reschedule(task)
 	case mesos_v1.TaskState_TASK_ERROR:
-		// TODO (tim): Error with the taskinfo sent to the agent. Give verbose reasoning back.
-		s.logger.Emit(logging.ERROR, message)
+		s.logger.Emit(logging.ERROR, "Error with task %s: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_FINISHED:
+		s.logger.Emit(
+			logging.INFO,
+			"Task %s on agent %s finished: %s",
+			taskIdVal,
+			agentIdVal,
+			message,
+		)
 		s.taskmanager.Delete(task)
 	case mesos_v1.TaskState_TASK_GONE:
 		// Agent is dead and task is lost.
-		s.logger.Emit(logging.ERROR, message)
+		s.logger.Emit(logging.ERROR, "Task %s is gone: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_GONE_BY_OPERATOR:
 		// Agent might be dead, master is unsure. Will return to RUNNING state possibly or die.
-		s.logger.Emit(logging.ERROR, message)
+		s.logger.Emit(logging.ERROR, "Task %s gone by operator: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_KILLED:
 		// Task was killed.
 		s.logger.Emit(
 			logging.INFO,
 			"Task %s on agent %s was killed",
-			taskId.GetValue(),
-			agentId.GetValue(),
+			taskIdVal,
+			agentIdVal,
 		)
 		s.taskmanager.Delete(task)
 	case mesos_v1.TaskState_TASK_KILLING:
 		// Task is in the process of catching a SIGNAL and shutting down.
-		s.logger.Emit(logging.INFO, message)
+		s.logger.Emit(logging.INFO, "Killing task %s: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_LOST:
 		// Task is unknown to the master and lost. Should reschedule.
-		s.logger.Emit(logging.ALARM, "Task %s was lost", taskId.GetValue())
+		s.logger.Emit(logging.ALARM, "Task %s was lost", taskIdVal)
 		s.reschedule(task)
 	case mesos_v1.TaskState_TASK_RUNNING:
-		s.logger.Emit(logging.INFO, message)
+		s.logger.Emit(
+			logging.INFO,
+			"Task %s on agent %s is running: %s",
+			taskIdVal,
+			agentIdVal,
+			message,
+		)
 	case mesos_v1.TaskState_TASK_STARTING:
-	// Task is still starting up. NOOP
+		// Task is still starting up. NOOP
+		s.logger.Emit(logging.INFO, "Task %s is starting: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_UNKNOWN:
-	// Task is unknown to the master. Should ignore.
+		// Task is unknown to the master. Should ignore.
+		s.logger.Emit(logging.ALARM, "Task %s is unknown: %s", taskIdVal, message)
 	case mesos_v1.TaskState_TASK_UNREACHABLE:
 		// Agent lost contact with master, could be a network error. No guarantee the task is still running.
 		// Should we reschedule after waiting a certain period of time?
-		s.logger.Emit(logging.INFO, message)
+		s.logger.Emit(logging.INFO, "Task %s is unreachable: %s", taskIdVal, message)
 	default:
 		// Somewhere in here the universe started.
 	}
