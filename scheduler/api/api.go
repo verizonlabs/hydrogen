e300eec925fead52fe3e20da546b84ca67997bf5
Aaron Wood <aaronjwood@gmail.com>
2017-05-31 18:25:09 -0400

    Simplify how the API is run by relying on the additional encapsulation from the server config. Apply routes in a cleaner way by using the restructured API components

--- scheduler/api/api.go
+++ scheduler/api/api.go
@@ -1,34 +1,12 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"mesos-framework-sdk/logging"
-	"mesos-framework-sdk/scheduler"
-	"mesos-framework-sdk/server"
 	"net/http"
 	"os"
+	sched "sprint/scheduler"
 	apiManager "sprint/scheduler/api/manager"
-	"strconv"
-)
-
-const (
-	baseUrl        = "/v1/api"
-	deployEndpoint = "/deploy"
-	statusEndpoint = "/status"
-	tasksEndpoint  = "/tasks"
-	killEndpoint   = "/kill"
-	updateEndpoint = "/update"
-
-	ACCEPTED = "Accepted"
-	LAUNCHED = "Launched"
-	FAILED   = "Failed"
-	RUNNING  = "Running"
-	KILLED   = "Killed"
-	NOTFOUND = "Not Found"
-	QUEUED   = "Queued"
-	UPDATE   = "Updated"
+	"sprint/scheduler/api/v1"
 )
 
 // Common format for API responses.
@@ -41,228 +19,50 @@ type Response struct {
 // API server used for scheduling/updating/killing tasks.
 // Provides an interface for users to interact with the core scheduler.
 type ApiServer struct {
-	cfg     server.Configuration
-	mux     *http.ServeMux
-	handle  map[string]http.HandlerFunc
-	sched   scheduler.Scheduler
+	cfg     *sched.Configuration
 	manager apiManager.ApiParser
-	version string
 	logger  logging.Logger
 }
 
 // Returns a new API server injected with the necessary components.
 func NewApiServer(
-	cfg server.Configuration,
+	cfg *sched.Configuration,
 	mgr apiManager.ApiParser,
-	mux *http.ServeMux,
-	version string,
 	lgr logging.Logger) *ApiServer {
 
-	return &ApiServer{
+	srv := &ApiServer{
 		cfg:     cfg,
-		handle:  make(map[string]http.HandlerFunc),
 		manager: mgr,
-		mux:     mux,
-		version: version,
 		logger:  lgr,
 	}
+
+	return srv
 }
 
-// Getter to return our map of handles.
-func (a *ApiServer) Handle() map[string]http.HandlerFunc {
-	return a.handle
-}
-
-// Set our default API handler routes here.
-func (a *ApiServer) setDefaultHandlers() {
-	a.handle[baseUrl+deployEndpoint] = a.deploy
-	a.handle[baseUrl+statusEndpoint] = a.state
-	a.handle[baseUrl+killEndpoint] = a.kill
-	a.handle[baseUrl+updateEndpoint] = a.update
-	a.handle[baseUrl+tasksEndpoint] = a.tasks
-}
-
-// Sets the optional custom HTTP handlers to be used in the API server.
-func (a *ApiServer) setHandlers(handles map[string]http.HandlerFunc) {
-	for route, handle := range handles {
-		a.handle[route] = handle
+func (a *ApiServer) applyRoutes(version string) {
+	switch version {
+	case "v1":
+		routes := v1.MapRoutes(v1.NewHandlers(a.manager))
+		for route, f := range routes {
+			a.cfg.APIServer.Server.Mux().HandleFunc(route, f)
+		}
 	}
 }
 
 // RunAPI sets up the various HTTP handlers, optionally configures TLS, and runs the server.
 func (a *ApiServer) RunAPI(handlers map[string]http.HandlerFunc) {
-	if handlers != nil || len(handlers) != 0 {
-		a.logger.Emit(logging.INFO, "Setting custom handlers.")
-		a.setHandlers(handlers)
-	} else {
-		a.logger.Emit(logging.INFO, "Setting default handlers.")
-		a.setDefaultHandlers()
-	}
+	a.applyRoutes(a.cfg.APIServer.Version)
+	apiSrvCfg := a.cfg.APIServer.Server
 
-	// Iterate through all methods and setup endpoints.
-	for route, handle := range a.handle {
-		a.mux.HandleFunc(route, handle)
-	}
-
-	if a.cfg.TLS() {
-		a.cfg.Server().Handler = a.mux
-		a.cfg.Server().Addr = ":" + strconv.Itoa(a.cfg.Port())
-		if err := a.cfg.Server().ListenAndServeTLS(a.cfg.Cert(), a.cfg.Key()); err != nil {
+	if apiSrvCfg.TLS() {
+		if err := apiSrvCfg.Server().ListenAndServeTLS(apiSrvCfg.Cert(), apiSrvCfg.Key()); err != nil {
 			a.logger.Emit(logging.ERROR, err.Error())
-			os.Exit(1)
+			os.Exit(7)
 		}
 	} else {
-		if err := http.ListenAndServe(":"+strconv.Itoa(a.cfg.Port()), a.mux); err != nil {
+		if err := apiSrvCfg.Server().ListenAndServe(); err != nil {
 			a.logger.Emit(logging.ERROR, err.Error())
-			os.Exit(1)
+			os.Exit(7)
 		}
 	}
-}
-
-// HTTP method guard to only allow specific methods on various endpoints.
-func (a *ApiServer) methodFilter(w http.ResponseWriter, r *http.Request, methods []string, ops func()) {
-	for _, method := range methods {
-		if method == r.Method {
-			ops()
-			return
-		}
-	}
-
-	json.NewEncoder(w).Encode(Response{
-		Status:  FAILED,
-		Message: r.Method + " is not allowed on this endpoint.",
-	})
-}
-
-// Deploy handler launches a given application from parsed JSON.
-func (a *ApiServer) deploy(w http.ResponseWriter, r *http.Request) {
-	a.methodFilter(w, r, []string{"POST"}, func() {
-		dec, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-
-		defer r.Body.Close()
-
-		task, err := a.manager.Deploy(dec)
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-
-		json.NewEncoder(w).Encode(Response{
-			Status:   QUEUED,
-			TaskName: task.GetName(),
-		})
-	})
-}
-
-// Update handler allows for updates to an existing/running task.
-func (a *ApiServer) update(w http.ResponseWriter, r *http.Request) {
-	a.methodFilter(w, r, []string{"PUT"}, func() {
-		dec, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-
-		defer r.Body.Close()
-
-		newTask, err := a.manager.Update(dec)
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-
-		json.NewEncoder(w).Encode(Response{
-			Status:  UPDATE,
-			Message: fmt.Sprintf("Updating %v", newTask.GetName()),
-		})
-	})
-}
-
-// Kill handler allows users to stop their running task.
-func (a *ApiServer) kill(w http.ResponseWriter, r *http.Request) {
-	a.methodFilter(w, r, []string{"DELETE"}, func() {
-		dec, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			return
-		}
-
-		defer r.Body.Close()
-
-		err = a.manager.Kill(dec)
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-		json.NewEncoder(w).Encode(Response{
-			Status:  KILLED,
-			Message: "Successfully killed task.",
-		})
-	})
-}
-
-// State handler provides the given task's current execution status.
-func (a *ApiServer) state(w http.ResponseWriter, r *http.Request) {
-	a.methodFilter(w, r, []string{"GET"}, func() {
-		name := r.URL.Query().Get("name")
-		if name == "" {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: "No name was found in URL params.",
-			})
-			return
-		}
-		state, err := a.manager.Status(name)
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-
-		json.NewEncoder(w).Encode(Response{Status: state.String(), TaskName: name})
-	})
-}
-
-// Tasks handler provides a list of all tasks known to the scheduler.
-func (a *ApiServer) tasks(w http.ResponseWriter, r *http.Request) {
-	a.methodFilter(w, r, []string{"GET"}, func() {
-		tasks, err := a.manager.AllTasks()
-		if err != nil {
-			json.NewEncoder(w).Encode(Response{
-				Status:  FAILED,
-				Message: err.Error(),
-			})
-			return
-		}
-
-		data := []Response{}
-		for _, t := range tasks {
-			data = append(data, Response{
-				Status:   t.State.String(),
-				TaskName: t.Info.GetName(),
-			})
-		}
-
-		json.NewEncoder(w).Encode(data)
-	})
 }
