c9c29896a46c8427c021063c07c815a15d24ffb1
Tim Hansen <timothy.hansen@one.verizon.com>
2017-06-21 11:24:26 -0400

    Fix bug where groups wouldn't be deleted or updated on kill requests.

--- scheduler/api/manager/apimanager.go
+++ scheduler/api/manager/apimanager.go
@@ -49,13 +49,6 @@ func (m *Parser) Deploy(decoded []byte) (*mesos_v1.TaskInfo, error) {
 		return nil, err
 	}
 
-	// If we have any filters, let the resource manager know.
-	if len(appJson.Filters) > 0 {
-		if err := m.resourceManager.AddFilter(mesosTask, appJson.Filters); err != nil {
-			return nil, err
-		}
-	}
-
 	// Deployment strategy
 	if appJson.Instances == 1 {
 		if err := m.taskManager.Add(mesosTask); err != nil {
@@ -65,6 +58,12 @@ func (m *Parser) Deploy(decoded []byte) (*mesos_v1.TaskInfo, error) {
 		if err != nil {
 			return nil, err
 		}
+		if len(appJson.Filters) > 0 {
+			if err := m.resourceManager.AddFilter(mesosTask, appJson.Filters); err != nil {
+				return nil, err
+			}
+		}
+
 	} else if appJson.Instances > 1 {
 		originalName := mesosTask.GetName()
 		if err := m.taskManager.CreateGroup(originalName); err != nil {
@@ -84,6 +83,11 @@ func (m *Parser) Deploy(decoded []byte) (*mesos_v1.TaskInfo, error) {
 			err := m.taskManager.AddPolicy(appJson.Retry, &duplicate)
 			if err != nil {
 				return nil, err
+			}
+			if len(appJson.Filters) > 0 {
+				if err := m.resourceManager.AddFilter(&duplicate, appJson.Filters); err != nil {
+					return nil, err
+				}
 			}
 		}
 	} else {
@@ -160,16 +164,25 @@ func (m *Parser) Kill(decoded []byte) (string, error) {
 
 	m.resourceManager.ClearFilters(tsk)
 
-	if *state == t.STAGING || *state == t.RUNNING || *state == t.STARTING || *state == t.UNKNOWN {
+	// If we are "unknown" that means the master doesn't know about the task, no need to make an HTTP call.
+	if *state != t.UNKNOWN {
 		_, err := m.scheduler.Kill(tsk.GetTaskId(), tsk.GetAgentId())
 		if err != nil {
 			return "", err
 		}
-
-		return *appJson.Name, nil
+	} else {
+		// we need to delete this task from a group since no event is generated
+		// by mesos if we kill a task before it's launched. For example,
+		// This occurs if we have a task that can't get a valid offer and sits in the queue.
+		// but we still want to remove it from the queue anyways.
+		if m.taskManager.IsInGroup(tsk) {
+			m.taskManager.Unlink(tsk.GetName(), tsk.GetAgentId())
+			m.taskManager.SetSize(tsk.GetName(), -1)
+			m.taskManager.DeleteGroup(tsk.GetName())
+		}
 	}
 
-	return "", errors.New("Task isn't staging, starting, or running")
+	return *appJson.Name, nil
 }
 
 func (m *Parser) Status(name string) (mesos_v1.TaskState, error) {
