5bc716962191469e88e810c3b72910668e912c50
Aaron Wood <aaronjwood@gmail.com>
2017-09-18 16:47:06 -0400

    Take in an interface, change to a generic MessageResponse struct, and set the correct content type

--- scheduler/api/v1/response.go
+++ scheduler/api/v1/response.go
@@ -20,31 +20,21 @@ import (
 )
 
 // v1 API response format.
-type Response struct {
-	TaskName string `json:"taskname,omitempty"`
-	Message  string `json:"message,omitempty"`
-	State    string `json:"state,omitempty"`
+type MessageResponse struct {
+	Message string `json:"message"`
 }
 
 var (
-	InternalServerError func(http.ResponseWriter, Response)   = responseFactory(http.StatusInternalServerError)
-	BadRequest          func(http.ResponseWriter, Response)   = responseFactory(http.StatusBadRequest)
-	MethodNotAllowed    func(http.ResponseWriter, Response)   = responseFactory(http.StatusMethodNotAllowed)
-	Success             func(http.ResponseWriter, Response)   = responseFactory(http.StatusOK)
-	MultiSuccess        func(http.ResponseWriter, []Response) = multiResponseFactory(http.StatusOK)
+	InternalServerError func(http.ResponseWriter, interface{}) = responseFactory(http.StatusInternalServerError)
+	BadRequest          func(http.ResponseWriter, interface{}) = responseFactory(http.StatusBadRequest)
+	MethodNotAllowed    func(http.ResponseWriter, interface{}) = responseFactory(http.StatusMethodNotAllowed)
+	Success             func(http.ResponseWriter, interface{}) = responseFactory(http.StatusOK)
 )
 
 // Creates a response function.
-func responseFactory(statusCode int) func(w http.ResponseWriter, r Response) {
-	return func(w http.ResponseWriter, r Response) {
-		w.WriteHeader(statusCode)
-		json.NewEncoder(w).Encode(r)
-	}
-}
-
-// Creates a response function that handles multiple responses.
-func multiResponseFactory(statusCode int) func(w http.ResponseWriter, r []Response) {
-	return func(w http.ResponseWriter, r []Response) {
+func responseFactory(statusCode int) func(w http.ResponseWriter, r interface{}) {
+	return func(w http.ResponseWriter, r interface{}) {
+		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(statusCode)
 		json.NewEncoder(w).Encode(r)
 	}
